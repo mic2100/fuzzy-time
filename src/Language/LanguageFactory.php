8045fb938888637961ee98197e8e266e43d93d31
Michael Bardsley <mic.bardsley@outlook.com>
2019-04-21 00:34:43 +0100

    Added the language factory

---
+++ src/Language/LanguageFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Mic2100\FuzzyTime\Language;
+
+use Exception;
+use InvalidArgumentException;
+use Mic2100\FuzzyTime\Language\Dictionaries\German;
+use Mic2100\FuzzyTime\Language\Dictionaries\English;
+
+class LanguageFactory
+{
+    /**
+     * Gets the language class based on the class handles
+     *
+     * @param string $language
+     *
+     * @return LanguageInterface
+     * @throws Exception - If the handle is not set on the language class
+     * @throws InvalidArgumentException - If an incorrect language is selected
+     */
+    public static function get(string $language): LanguageInterface
+    {
+        switch ($language) {
+            case English::HANDLE:
+                return new English();
+
+            case German::HANDLE:
+                return new German();
+
+            default:
+                throw new InvalidArgumentException(
+                    sprintf('An invalid language was selected: %s', var_export($language, true))
+                );
+        }
+    }
+}
